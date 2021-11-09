3806665d13227e724d4e72d166e9ebd44bf695bd
Cong Hao <77195793+hori2012@users.noreply.github.com>
2021-11-09 08:44:37 +0700

    Create BTL-2-Ki1.cpp

---
+++ BTL-2-Ki1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Profile{
+	private:
+		string  fullname, mssv, province, Class;
+		float gpa;
+	public:
+		friend istream& operator >> (istream& is, Profile &pf1);
+		friend ostream& operator << (ostream& os, Profile &pf1);
+		float getGpa();
+		string getName();
+		void set_Gpa() ;
+};
+
+istream& operator >> (istream& is, Profile &pf1){
+	cout<<"Nhap ho ten sinh vien: ";
+	fflush(stdin);
+	getline(is, pf1.fullname);
+	cout<<endl;
+	cout<<"Nhap ma so sinh vien: ";
+	fflush(stdin);
+	getline(is, pf1.mssv);
+	cout<<endl;
+	cout<<"Nhap lop: ";
+	fflush(stdin);
+	getline(is, pf1.Class);
+	cout<<endl;
+	cout<<"Nhap que cua sinh vien: ";
+	fflush(stdin);
+	getline(is, pf1.province);
+	cout<<endl;
+	cout<<"Nhap diem trung binh: ";
+	is>>pf1.gpa;
+	cout<<endl;
+	return is;
+}
+ostream& operator << (ostream& os, Profile &pf1){
+	os<<"Ho va ten: "<<pf1.fullname<<endl;
+	os<<"MSSV: "<<pf1.mssv<<endl;
+	os<<"Lop: "<<pf1.Class<<endl;
+	os<<"Que: "<<pf1.province<<endl;
+	os<<"Diem trung binh: "<<pf1.gpa<<endl;
+	return os;
+}
